4b3d716718071de5b093f0b288d5a4d5149121bd
damarion989 <d.butlers282@gmail.com>
2026-09-06 22:01:01 -0500

    Update PLaninText.java

--- PLaninText.java
+++ PLaninText.java
@@ -1,1 +1,12 @@
+public class PlainText Implements TxtComponent {
+  privte String text;
 
+  public PlainText(String text ) {
+    this.text = text;
+  }
+
+  @Overrride
+  public String getText() {
+    return text;
+  }
+}
