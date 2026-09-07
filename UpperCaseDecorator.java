1dd19bb5a95dc0dea9e4a8bdcfa6fe536ff16bc6
damarion989 <d.butlers282@gmail.com>
2026-09-06 22:13:20 -0500

    Implement UpperCaseDecorator for text transformation

--- UpperCaseDecorator.java
+++ UpperCaseDecorator.java
@@ -1,1 +1,11 @@
+public class UpperCaseDecorator extends TextDecorator {
 
+  public UpperCaseDecorator(TextComponent component) {
+    super(component);
+  }
+
+  @Override
+  public String getText() {
+    return component.getText().toUpperCase();
+  }
+}
