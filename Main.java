a0be5b69e3f35a75314ba8a39c856a10886d1e43
damarion989 <d.butlers282@gmail.com>
2026-09-06 22:19:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
 
+        TextComponent plain = new PlainText("Hello World");
+        System.out.println("Plain: " + plain.getText());
+
+        TextComponent bold = new BoldDecorator(
+                new PlainText("Hello World"));
+        System.out.println("Bold: " + bold.getText());
+
+        TextComponent boldItalic = new ItalicDecorator(
+                new BoldDecorator(
+                        new PlainText("Hello World")));
+        System.out.println("Bold and Italic: " + boldItalic.getText());
+
+        TextComponent allDecorators = new UpperCaseDecorator(
+                new ItalicDecorator(
+                        new BoldDecorator(
+                                new PlainText("Hello World"))));
+        System.out.println("All Decorators: " + allDecorators.getText());
+    }
+}
